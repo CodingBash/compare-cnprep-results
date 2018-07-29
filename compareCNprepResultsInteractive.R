35ac99cf1f8a09089d82fb9764100ae26b1b2f71
CodingBash <bbecerr@outlook.com>
2018-07-29 10:30:46 -0400

    Updated examples and notebook documentation

--- compareCNprepResultsInteractive.R
+++ compareCNprepResultsInteractive.R
@@ -5,24 +5,66 @@ setwd("~/Git-Projects/Git-Research-Projects/compare-cnprep-results/") # Set work
 source("compareCNprepResultsLibrary.R") # Import visualization library
 library(RColorBrewer) # Import brewer for coloring
 
-#
-# Set colors
-#
 cluster_cols <- brewer.pal(n = 7, name="Dark2") # Set colors for clusters. Let n > 5
 supplementary_cols <- brewer.pal(n = 7, name="Set2") # Set colors for suppl. Let n >= number of suppl values
 
-#
-# See available CNprep runs
-#
-print(all_model_specs)
-#
-# Select the CNprep specs to compare with
-#
-selected_model_specs <- c(1,15)
+options(repr.plot.width=15, repr.plot.height=7)
+options(warn=-1)
 
-#
-# Create target_segments_function. Script will apply over segments and use this FUN as selection criteria
-#
+
+print(all_model_specs)
+
+selected_model_specs <- c(1,15)
+print(all_model_specs[selected_model_specs,])
+
+organoidId <- "hT1"
+
+supplementary_values <- c("seg.median")
+grid_lines <- TRUE
+chrom_lines <- TRUE
+bin_coord <- FALSE
+
+displayCNprepResults(organoidId= organoidId, model_specs = all_model_specs[selected_model_specs, ], supplementary_values = c("seg.median"), supplementary_cols =  supplementary_cols, grid_lines = grid_lines, chrom_lines = chrom_lines, bin_coord = bin_coord)
+
+cluster_value = "maxzmean"
+grid_lines <- TRUE
+chrom_lines <- TRUE
+bin_coord <- FALSE
+
+displayCNprepResults(organoidId= organoidId, model_specs = all_model_specs[selected_model_specs, ], cluster_value = cluster_value , cluster_cols = cluster_cols, grid_lines = grid_lines, chrom_lines = chrom_lines, bin_coord = bin_coord)
+
+cluster_value = "maxzmean"
+clustered_supplementary_value = "mediandev"
+overlay_cluster_means = TRUE
+grid_lines <- TRUE
+chrom_lines <- TRUE
+bin_coord <- FALSE
+
+displayCNprepResults(organoidId= organoidId, model_specs = all_model_specs[selected_model_specs, ], cluster_value = cluster_value, clustered_supplementary_value = clustered_supplementary_value, overlay_cluster_means = overlay_cluster_means, cluster_cols = cluster_cols, grid_lines = grid_lines, chrom_lines = chrom_lines, bin_coord = bin_coord)
+
+start = 1e9
+end = 2.5e9
+
+cluster_value = "maxzmean"
+clustered_supplementary_value = "mediandev"
+overlay_cluster_means = TRUE
+grid_lines <- TRUE
+chrom_lines <- TRUE
+bin_coord <- FALSE
+
+displayCNprepResults(organoidId= organoidId, start = start, end = end, model_specs = all_model_specs[selected_model_specs, ], cluster_value = cluster_value, clustered_supplementary_value = clustered_supplementary_value, overlay_cluster_means = overlay_cluster_means, cluster_cols = cluster_cols, grid_lines = grid_lines, chrom_lines = chrom_lines, bin_coord = bin_coord)
+
+select_chrom = 3
+
+cluster_value = "maxzmean"
+clustered_supplementary_value = "mediandev"
+overlay_cluster_means = TRUE
+grid_lines <- TRUE
+chrom_lines <- TRUE
+bin_coord <- FALSE
+
+displayCNprepResults(organoidId= organoidId, select_chrom = select_chrom, model_specs = all_model_specs[selected_model_specs, ], cluster_value = cluster_value, clustered_supplementary_value = clustered_supplementary_value, overlay_cluster_means = overlay_cluster_means, cluster_cols = cluster_cols, grid_lines = grid_lines, chrom_lines = chrom_lines, bin_coord = bin_coord)
+
 highlight_facets_events <- function(segment){
   result <- segment[["seg.median"]] > 0.20 || segment[["seg.median"]] < -0.235
   return(result)
@@ -32,24 +74,24 @@ highlight_cnprep_events <- function(segment){
   return(result)
 }
 
-#
-# Example 1: Use the target_segments_function to highlight aberration events
-#
-# See the events that CNprep calls
-displayCNprepResults(organoidId= "hT1", model_specs = all_model_specs[selected_model_specs, ],cluster_cols = cluster_cols, grid_lines = TRUE, chrom_lines = TRUE, bin_coord = FALSE,  target_segments_function = highlight_cnprep_events, target_segments_value = "seg.median", target_segments_col = "maroon", supplementary_values = c("seg.median"), supplementary_cols = supplementary_cols)
-# See the events that FACETs calls
-displayCNprepResults(organoidId= "hT1", model_specs = all_model_specs[selected_model_specs, ],cluster_cols = cluster_cols, grid_lines = TRUE, chrom_lines = TRUE, bin_coord = FALSE,  target_segments_function = highlight_facets_events, target_segments_value = "seg.median", target_segments_col = "maroon", supplementary_values = c("seg.median"), supplementary_cols = supplementary_cols)
+target_segments_function = highlight_cnprep_events
+target_segments_value = "seg.median"
+target_segments_col = "maroon"
+supplementary_values = c("seg.median")
 
-# Example 2: Display CNprep results for organoid "hT1" for CNprep runs #1,12.
-# See what cluster each segmedian is assigned to - view the cluster mean as well
-displayCNprepResults(organoidId= "hT105", model_specs = all_model_specs[selected_model_specs, ], cluster_value = "maxzmean", clustered_supplementary_value = "mediandev", overlay_cluster_means = TRUE, cluster_cols = cluster_cols, supplementary_cols =  supplementary_cols, grid_lines = FALSE, chrom_lines = TRUE, bin_coord = FALSE)
-# Display in specific range (make sure range is consistent with x units selected)
-displayCNprepResults(organoidId= "hT1", start = 1000000000, end = 2500000000, model_specs = all_model_specs[selected_model_specs, ], cluster_value = "maxzmean", clustered_supplementary_value = "mediandev", overlay_cluster_means = TRUE, cluster_cols = cluster_cols, supplementary_cols =  supplementary_cols, grid_lines = TRUE, chrom_lines = TRUE, bin_coord = FALSE)
-# Display in specific chromosome
-displayCNprepResults(organoidId= "hT1",select_chrom = 8, model_specs = all_model_specs[selected_model_specs, ], cluster_value = "maxzmean", clustered_supplementary_value = "mediandev", overlay_cluster_means = TRUE, cluster_cols = cluster_cols, supplementary_cols =  supplementary_cols, grid_lines = TRUE, chrom_lines = TRUE, bin_coord = FALSE)
-# Display original with no layout lines
-displayCNprepResults(organoidId= "hT1", model_specs = all_model_specs[selected_model_specs, ], cluster_value = "maxzmean", clustered_supplementary_value = "mediandev", overlay_cluster_means = TRUE, cluster_cols = cluster_cols, supplementary_cols =  supplementary_cols, bin_coord = FALSE)
-# Display in bin units
-displayCNprepResults(organoidId= "hT1", model_specs = all_model_specs[selected_model_specs, ], cluster_value = "maxzmean", clustered_supplementary_value = "mediandev", overlay_cluster_means = TRUE, cluster_cols = cluster_cols, supplementary_cols =  supplementary_cols, bin_coord = TRUE)
-# Display within bin range
-displayCNprepResults(organoidId= "hT1", start = 20000, end = 60000, model_specs = all_model_specs[selected_model_specs, ], cluster_value = "maxzmean", clustered_supplementary_value = "mediandev", overlay_cluster_means = TRUE, cluster_cols = cluster_cols, supplementary_cols =  supplementary_cols, bin_coord = TRUE)
+grid_lines <- TRUE
+chrom_lines <- TRUE
+bin_coord <- FALSE
+
+displayCNprepResults(organoidId= organoidId, model_specs = all_model_specs[selected_model_specs, ], target_segments_function = target_segments_function, target_segments_value = target_segments_value, target_segments_col = target_segments_col, supplementary_values = supplementary_values , supplementary_cols = supplementary_cols, grid_lines = grid_lines, chrom_lines = chrom_lines, bin_coord = bin_coord)
+
+target_segments_function = highlight_facets_events
+target_segments_value = "seg.median"
+target_segments_col = "maroon"
+supplementary_values = c("seg.median")
+
+grid_lines <- TRUE
+chrom_lines <- TRUE
+bin_coord <- FALSE
+
+displayCNprepResults(organoidId= organoidId, model_specs = all_model_specs[selected_model_specs, ], target_segments_function = target_segments_function, target_segments_value = target_segments_value, target_segments_col = target_segments_col, supplementary_values = supplementary_values , supplementary_cols = supplementary_cols, grid_lines = grid_lines, chrom_lines = chrom_lines, bin_coord = bin_coord)
